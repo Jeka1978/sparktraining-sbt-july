f0841073120d29aacd57dc56c10f5f1a7c916bc8
bsevgeny@gmail.com <papakita2009>
2017-07-19 17:01:51 +0300

    env configured

--- src/main/java/taxi/Main.java
+++ src/main/java/taxi/Main.java
@@ -1,5 +1,6 @@
 package taxi;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -7,6 +8,7 @@ import org.apache.spark.api.java.JavaSparkContext;
 /**
  * Created by Evegeny on 19/07/2017.
  */
+@Slf4j
 public class Main {
     public static void main(String[] args) {
         SparkConf sparkConf = new SparkConf().setMaster("local[*]").setAppName("taxi");
@@ -14,5 +16,6 @@ public class Main {
         JavaRDD<String> rdd = sc.textFile("data/taxi_order.txt");
         long count = rdd.count();
         System.out.println("count = " + count);
+        log.warn("asdasdas");
     }
 }
